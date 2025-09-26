44866c28b8acbc1bd85fb93e543d8dfa47d6a0af
Your Name <you@example.com>
2025-09-25 21:36:24 -0300

    feat: Inclui Header na página inicial

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
-function home() {
+import Header from "../components/Header";
+
+function Home() {
   return (
     <div>
-        <h1>Ola Mundo </h1>
+      <Header />
+      <h1>Ola Mundo</h1>
     </div>
-
   );
 }
 
